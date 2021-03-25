3f18b12a44b0aeac354f3ad8e92332341baaa0d7
Monsrud Cooper <817455@stma.k12.mn.us>
2021-03-25 10:27:41 -0500

    topFiveMoviesTry3

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def topFiveMovies(a, b, c, d, e)
+def topFiveMovies(a, b, c, d, e):
     return a + b + c + d + e
 a = "Shrek 1 "
 b = "Shrek 2 "
 c = "Shrek 3 "
 d = "Shrek 4 "
 e = "Shrek 5 "
-
-print(topFiveMovies(a, b, c, d, e)
+topFiveMovies(a, b, c, d, e)
+print(topFiveMovies(a, b, c, d, e))
