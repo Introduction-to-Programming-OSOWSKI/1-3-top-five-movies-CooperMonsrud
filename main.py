83805552769ff44c0b963ad0eb31e9e0ee77479a
Monsrud Cooper <817455@stma.k12.mn.us>
2021-03-25 10:21:17 -0500

    topFiveMoviesTry2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def topFiveMovies(a, b, c, d, e)
     return a + b + c + d + e
-a = "Shrek 1"
-b = "Shrek 2"
-c = "Shrek 3"
-d = "Shrek 4"
-e = "Shrek 5"
+a = "Shrek 1 "
+b = "Shrek 2 "
+c = "Shrek 3 "
+d = "Shrek 4 "
+e = "Shrek 5 "
 
-topFiveMovies(a, b, c, d, e)
+print(topFiveMovies(a, b, c, d, e)
