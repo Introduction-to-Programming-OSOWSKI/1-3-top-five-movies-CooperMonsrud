23ccb66ac71a43e04244496a967fa3bf6d80039c
Monsrud Cooper <817455@stma.k12.mn.us>
2021-03-25 10:16:45 -0500

    topFivMoviesTry1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e)
+    return a + b + c + d + e
+a = "Shrek 1"
+b = "Shrek 2"
+c = "Shrek 3"
+d = "Shrek 4"
+e = "Shrek 5"
+
+topFiveMovies(a, b, c, d, e)
